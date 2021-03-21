e5a0e6a8c77949558b1fc8569220984f83b1cef0
PETROXX <49368785+PETROXX@users.noreply.github.com>
2021-03-21 15:07:28 +0300

    HT2 fix
    Improved spawn mechanism

--- Assets/Scripts/HT2/EnemySpawner.cs
+++ Assets/Scripts/HT2/EnemySpawner.cs
@@ -5,6 +5,7 @@ using System.Linq;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField] private SpawnTypes SpawnType;
     [SerializeField] private GameObject _enemyPrefab;
     [SerializeField] private Transform _spawnPointsHandler;
     [SerializeField] private List<Transform> _spawnPoints;
@@ -16,17 +17,52 @@ public class EnemySpawner : MonoBehaviour
     {
         _spawnPoints = _spawnPointsHandler.GetComponentsInChildren<Transform>().ToList();
         _spawnPoints.Remove(_spawnPointsHandler);
-        StartCoroutine(InstantiateEnemy());
+        switch(SpawnType)
+        {
+            case SpawnTypes.InfiniteRandom:
+                StartCoroutine(InfiniteRandomSpawn());
+                break;
+            case SpawnTypes.InfiniteSequential:
+                StartCoroutine(InfiniteSequentialSpawn());
+                break;
+            case SpawnTypes.LimitedSequential:
+                StartCoroutine(LimitedSequentialSpawn());
+                break;
+        }
+
     }
 
-    IEnumerator InstantiateEnemy()
+    IEnumerator LimitedSequentialSpawn()
     {
         Instantiate(_enemyPrefab, _spawnPoints[_spawnIndex].position, Quaternion.identity);
         _spawnIndex++;
         if(_spawnIndex <= _spawnPoints.Count - 1)
         {
             yield return new WaitForSeconds(_cooldown);
-            StartCoroutine(InstantiateEnemy());
+            StartCoroutine(LimitedSequentialSpawn());
         }
+    }
+
+    IEnumerator InfiniteRandomSpawn()
+    {
+        Instantiate(_enemyPrefab, _spawnPoints[Random.Range(0, _spawnPoints.Count)].position, Quaternion.identity);
+        yield return new WaitForSeconds(_cooldown);
+        StartCoroutine(InfiniteRandomSpawn());
+    }
+
+    IEnumerator InfiniteSequentialSpawn()
+    {
+        Instantiate(_enemyPrefab, _spawnPoints[_spawnIndex].position, Quaternion.identity);
+        _spawnIndex++;
+        if (_spawnIndex > _spawnPoints.Count - 1) _spawnIndex = 0;
+        yield return new WaitForSeconds(_cooldown);
+        StartCoroutine(InfiniteSequentialSpawn());
+    }
+
+    enum SpawnTypes
+    {
+        InfiniteRandom,
+        InfiniteSequential,
+        LimitedSequential
     }
 }
